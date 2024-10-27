ac7acd72264cebde0010d1fd05b8c284af9a5339
JyothikSandeep <101253647+JyothikSandeep@users.noreply.github.com>
2024-10-27 21:40:54 +0530

    again added verel URl

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app=express()
 const Registerrouter=require("./routes/RegistrationRoute")
 PORT=8000
 app.use(express.json());
-app.use("https://pitch-venture-backend.vercel.app/", Registerrouter);
+app.use(
+  "https://pitch-venture-backend-gm6ce63lr-jyothiks-projects.vercel.app/",
+  Registerrouter
+);
 // app.get("/hello",(req,res)=>{
 //     console.log("insidde Hello")
 //     res.send("Hello")
